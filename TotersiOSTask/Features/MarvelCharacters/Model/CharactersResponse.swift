5df9ddfcbadba0f8ccbd56abffc651c641cb5d2d
ZeyadElassal4793 <55943235+Zeyadelassal@users.noreply.github.com>
2022-09-24 21:33:21 +0200

    add characters response

---
+++ TotersiOSTask/Features/MarvelCharacters/Model/CharactersResponse.swift
@@ -0,0 +1,43 @@
+//
+//  CharactersResponse.swift
+//  TotersiOSTask
+//
+//  Created by Zeyad Elassal on 24/09/2022.
+//
+
+import Foundation
+
+struct CharactersResponse: APIResponse {
+    var code: Int
+    var message: String?
+    var data: CharactersResponseData?
+}
+
+struct CharactersResponseData: Codable {
+    let count: Int?
+    let total: Int?
+    let characters: [Character]?
+    
+    enum CodingKeys: String, CodingKey {
+        case count
+        case total
+        case characters = "result"
+    }
+}
+
+struct Character: Codable {
+    let id: Int?
+    let name: String?
+    let thumbnail: Thumbnail?
+}
+
+struct Thumbnail: Codable {
+    let path: String?
+    let thumbnailExtension: String
+    
+    enum CodingKeys: String, CodingKey {
+        case path
+        case thumbnailExtension = "extension"
+    }
+}
+
