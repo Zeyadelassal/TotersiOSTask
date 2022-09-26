94cc3faf73172b7680c18da32cd9007dfdb31562
ZeyadElassal4793 <55943235+Zeyadelassal@users.noreply.github.com>
2022-09-26 17:02:00 +0200

    add character details router

---
+++ TotersiOSTask/Features/MarvelCharacterDetails/Router/CharacterDetailsRouter.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterDetailsRouter.swift
+//  TotersiOSTask
+//
+//  Created by Zeyad Elassal on 26/09/2022.
+//
+
+import UIKit
+
+class CharacterDetailsRouter: CharacterDetailsRouterProtocol {
+    
+    weak var view: UIViewController?
+    
+}
---
+++ TotersiOSTask/Features/MarvelCharacterDetails/Router/CharacterDetailsRouterProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterDetailsRouterProtocol.swift
+//  TotersiOSTask
+//
+//  Created by Zeyad Elassal on 26/09/2022.
+//
+
+import Foundation
+
+protocol CharacterDetailsRouterProtocol: AnyObject {
+    
+}
